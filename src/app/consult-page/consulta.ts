ddfa3b21e77dcfd3bb9138bc13b7fc8f65c5a218
lucas-koiti <lucaskoitig@gmail.com>
2019-05-28 22:01:04 -0300

    alteracoes do html

---
+++ src/app/consult-page/consulta.ts
@@ -0,0 +1,9 @@
+export class Consulta {
+  constructor(
+    public crm: number = 0,
+    public cpf: number = 0,
+    public hora_da_consulta: number = 0,
+    public sala: number = 0,
+    public data_da_consulta : number = 0
+  ){}
+}
