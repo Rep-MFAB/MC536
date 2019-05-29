dbd0d5e0c2d0af000ac47efaa25569da08cd7f5b
LuizZucchi <luiz.e.a.zucchi@gmail.com>
2019-05-28 21:11:29 -0300

    first

---
+++ src/app/consult-page/atendimentops.ts
@@ -0,0 +1,8 @@
+export class Atendimentops {
+  constructor(
+    public protocolo_atendimento: number = 0,
+    public data_atendimento: number = 0,
+    public tipo_de_socorro: string = "",
+    public cpf: string = ""
+  ){}
+}
---
+++ src/app/consult-page/cirurgia.ts
@@ -0,0 +1,10 @@
+export class Cirurgia {
+  constructor(
+    public protocolo_cirurgia: number = 0,
+    public hora_da_cirurgia: number = "",
+    public data_da_cirurgia: string = "",
+    public tipo_da_cirurgia: string = "",
+    public cpf: number = 0,
+    public crm: number = 0
+  ){}
+}
